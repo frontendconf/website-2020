6108bce35e48b89bac1ac8866b76cefbbe9ac06e
Thomas Jaggi <thomas@responsive.ch>
2019-11-30 22:04:44 +0100

    extend Contentful setup

---
+++ config.js
@@ -0,0 +1,7 @@
+export default {
+  // API preview: https://graphql.contentful.com/content/v1/spaces/j8md9ikrag2j/explore?access_token=c0eae4b4c6ddb1acdef7590927e9c901eca36f51fffd93a540a13a756d3278a4
+  contentful:
+    'https://graphql.contentful.com/content/v1/spaces/j8md9ikrag2j?access_token=c0eae4b4c6ddb1acdef7590927e9c901eca36f51fffd93a540a13a756d3278a4',
+  newsletter:
+    '//frontendconf.us2.list-manage.com/subscribe/post?u=d6e0840333568eaec22d009ab&amp;id=d822473667',
+}
